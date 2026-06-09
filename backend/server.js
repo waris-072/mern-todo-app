889e1aa960f677088ad0dcadab94bed4ef7eba38
waris-072 <warishakrow3@gmail.com>
2026-06-09 17:07:59 +0500

    fix: render deployment port support

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ connectDB();
 const todoRoutes = require("./routes/todoRoutes");
 app.use("/todos", todoRoutes);
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
